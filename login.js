39df1ee0f8668dcef19c6be74c7a6550b64637e7
mdsabbirvuia <mdsabbir3225@gmail.com>
2026-03-09 20:05:23 +0600

    logging In loading add make smother experiencec

--- login.js
+++ login.js
@@ -1,10 +1,20 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
+const singIn = document.getElementById("signin");
+const loggingIn = document.getElementById("loggingin");
+
 document.getElementById("btn-login").addEventListener("click", () => {
 
     if (username.value === "admin" && password.value === "admin123") {
+        singIn.classList.add("hidden");
+        loggingIn.classList.remove("hidden")
+
+        setTimeout(()=>{
+        
         window.location.href = "./home.html";
+        }, 1000)
+
     } else {
         alert("Invalid username or password");
     }
